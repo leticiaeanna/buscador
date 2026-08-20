d2ffe5607a08a75a9c8b066ae88b270f8134435e
leticia mendes <leticiaeanna678@gmail.com>
2026-08-20 12:14:41 -0300

    ovo

--- meu.js
+++ meu.js
@@ -1,11 +1,2 @@
- const nome = document.getElementById("nome")
-
-async function buscar(){
- alert ("Minha buscar funciona! Oba!")
- const response = await fetch(`https://viacep.com.br/ws/${55014460}/json/`)
- let endereco = await response.json()
- console.log(inputNome.value)
-}
-function enviar(){
- alert ("uhuuull")
- 
+const ovo = document.getElementById("ovo")
+ovo.value = "Este é uma frase"
